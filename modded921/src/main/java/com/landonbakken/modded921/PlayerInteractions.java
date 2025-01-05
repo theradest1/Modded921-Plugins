ffaa6d8809103b07a214658a01ca89ef7eae19f0
TheRad1 <landon.bakken@gmail.com>
2025-01-04 18:02:30 -0600

    got rid of useless thing

--- modded921/src/main/java/com/landonbakken/modded921/PlayerInteractions.java
+++ modded921/src/main/java/com/landonbakken/modded921/PlayerInteractions.java
@@ -1,11 +1,10 @@
 package com.landonbakken.modded921;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerInteractions implements Listener {
@@ -24,8 +23,8 @@ public class PlayerInteractions implements Listener {
 			onLeftClick(event);
 			//stop normal things from happening
 			event.setCancelled(true);
-			}
-			if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+		}
+		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			//right click
 			onRightClick(event);
 			//stop normal things from happening
@@ -35,19 +34,11 @@ public class PlayerInteractions implements Listener {
 
 	@EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        // Check if the damager is a player
-        if (event.getDamager() instanceof Player) {
-            // Cancel the event to prevent the attack
-            event.setCancelled(true);
-            // Optionally, notify the player
-            ((Player) event.getDamager()).sendMessage("You cannot attack mobs!");
-        }
+        //do things
     }
 
 	@EventHandler
-    public void onPlayerAnimation(PlayerAnimationEvent event) {
-        if (event.getPlayer() instanceof Player) {
-            event.setCancelled(true);  // Cancel the animation
-        }
-    }
+	public void onEntityDamage(EntityDamageEvent event) {
+		//do things
+	}
 }
